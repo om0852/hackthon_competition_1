6303fe88229c1e093987f3c11d6396da93ba1e67
Hrushi4151 <hrushimore4151@gmail.com>
2024-03-15 15:58:17 +0530

    2nd fornrtgg

--- certificate/src/app/components/Slider.js
+++ certificate/src/app/components/Slider.js
@@ -1,45 +1,34 @@
-'use client';
-import react, { useEffect, useState } from 'react'
-import Link from 'next/link'
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import React from 'react';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
 
 const Slider = () => {
-    const [popularMovies, setpopularMovies] = useState([])
-    //     useEffect(() => {
-    //         fetch("https://api.themoviedb.org/3/movie/popular?api_key=fca8a4b893c170d5868618d48948fdf2")
-    //             .then(res => res.json())
-    //             .then(data => setpopularMovies(data.results))
+    const images = [
+        "https://source.unsplash.com/random/?saiababa",
+        "https://source.unsplash.com/random/?saiababa",
+        "https://source.unsplash.com/random/?saiababa",
+       
+    ];
 
-
-    // },[])
     return (
-        <></>
-        // <Carousel
-        //     showThumbs={true}
-        //     autoPlay={true}
-        //     transitionTime={2}
-        //     infiniteLoop={true}
-        //     showStatus={false}
-        //     className="w-100 h-100"
-        // >
-        //     {
+        <Slide className="w-[80vw] h-[50vh]">
+            <div className="each-slide-effect w-[80vw] h-[50vh] ">
+                <div className="w-[80vw] h-[50vh] flex justify-center items-center " style={{ 'backgroundImage': `url(${images[0]})` }}>
+                    <span>Slide 1</span>
+                </div>
+            </div>
+            <div  className="each-slide-effect w-[80vw] h-[50vh]">
+                <div className="w-[80vw] h-[50vh] flex justify-center items-center" style={{ 'backgroundImage': `url(${images[1]})` }}>
+                    <span>Slide 2</span>
+                </div>
+            </div>
+            <div className="each-slide-effect w-[80vw] h-[50vh]">
+                <div className="w-[80vw] h-[50vh]" style={{ 'backgroundImage': `url(${images[2]})` }}>
+                    <span>Slide 3</span>
+                </div>
+            </div>
+        </Slide>
+    );
+};
 
-        //         popularMovies.map((elem, index) => {
-        //             return (
-        //                 <Link key={index} href={`/products`}>
-        //                     <div>
-        //                         <img src={`https://image.tmdb.org/t/p/original${elem && elem.backdrop_path
-        //                             }`} />
-        //                         <p className="legend"> {elem ? elem.original_title : ""}</p>
-        //                     </div>
-
-        //                 </Link>
-        //             )
-        //         })
-        //     }
-        // </Carousel>
-    )
-}
-
-export default Slider
+export default Slider;
